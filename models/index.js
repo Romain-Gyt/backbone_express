7f8117c3bf543ebddfb274678b331624aa7121c9
Frame <guyenot.romain@protonmail.com>
2024-08-30 00:53:15 +0200

    configuration de la db

--- models/index.js
+++ models/index.js
@@ -10,26 +10,37 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
+
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  // Utiliser la variable d'environnement pour la chaîne de connexion
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    dialect: 'postgres',
+    ...config
+  });
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  // Utiliser les informations de configuration fournies directement
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: 'postgres',
+    port: config.port,
+    ...config
+  });
 }
 
 fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+    .readdirSync(__dirname)
+    .filter(file => {
+      return (
+          file.indexOf('.') !== 0 &&
+          file !== basename &&
+          file.slice(-3) === '.js' &&
+          file.indexOf('.test.js') === -1
+      );
+    })
+    .forEach(file => {
+      const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+      db[model.name] = model;
+    });
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
