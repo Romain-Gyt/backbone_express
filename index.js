3898426e5223fd15ecd4e7e483571f4c85c47482
Frame <162555287+Romain-Gyt@users.noreply.github.com>
2024-09-09 15:23:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000 ;  // Défini le port à partir de l'environnement ou utilise le port 4000 par défaut
 
 // Middleware
-app.use(cors());  // Activer les CORS
+app.use(cors());  // Active les CORS
 app.use(express.json());  // Pour pouvoir lire les données JSON dans les requêtes
 
 //
@@ -32,6 +32,6 @@ seed.runInitialSeed().then(() => {
     });
 }).catch(err => {
     console.error('Failed to run initial seed:', err);
-    process.exit(1); // Quitter le processus en cas d'erreur
+    process.exit(1); // Quitte le processus en cas d'erreur
 })
 
